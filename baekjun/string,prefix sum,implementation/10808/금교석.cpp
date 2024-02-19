a051aa2b1d7ae1985a9d41f28f536201ec939edb
player31-kks <keumks789@naver.com>
2024-02-20 00:07:42 +0900

    add second week solutions(금교석)

---
+++ baekjun/string,prefix sum,implementation/10808/금교석.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+int cnt[26];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> str;
+    for (char a : str)
+    {
+        cnt[a - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+        cout << cnt[i] << " ";
+
+    return 0;
+}
